2a55bcf0bcf5a1e2aa7cc7711ee83ee5f2ccbd8d
Saucy Enchiladas <so@Saucys-MacBook-Air.local>
2020-12-10 12:31:04 -0600

    fix

--- routes/token.js
+++ routes/token.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var btoa = require('btoa');
 
 var clientId = process.env.SPOT_ID,
   clientSecret = process.env.SPOT_SECRET;
