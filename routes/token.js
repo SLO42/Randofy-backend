4621816ef9bc8e8b9db399466bc7f420138cbc43
SLO42 <saolivei@student.42.us.org>
2020-12-26 16:45:14 -0600

    token

--- routes/token.js
+++ routes/token.js
@@ -5,7 +5,7 @@ var axios = require('axios');
   /* GET home page. */
   router.get('/', function(req, res, next) {
     const code = req.query[0];
-    console.log(code);
+    console.log("url: ", `grant_type=client_credentials&code=${encodeURIComponent(code)}&redirect_uri=${decodeURIComponent("https://randofy.vercel.app")}&client_id=${req.spotify._credentials.clientId}&client_secret=${req.spotify._credentials.clientSecret}`);
     axios.post("https://accounts.spotify.com/api/token",
     `grant_type=client_credentials&code=${encodeURIComponent(code)}&redirect_uri=${decodeURIComponent("https://randofy.vercel.app")}&client_id=${req.spotify._credentials.clientId}&client_secret=${req.spotify._credentials.clientSecret}`,
     {
