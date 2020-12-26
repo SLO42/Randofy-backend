15ea18a272d672744bbef4b66b94275391da5a74
SLO42 <saolivei@student.42.us.org>
2020-12-26 16:53:52 -0600

    token added query

--- routes/token.js
+++ routes/token.js
@@ -4,7 +4,7 @@ var axios = require('axios');
 
   /* GET home page. */
   router.get('/', function(req, res, next) {
-    const code = req.query[0];
+    const code = req.query.code;
     console.log("url: ", `grant_type=client_credentials&code=${encodeURIComponent(code)}&redirect_uri=${decodeURIComponent("https://randofy.vercel.app")}&client_id=${req.spotify._credentials.clientId}&client_secret=${req.spotify._credentials.clientSecret}`);
     axios.post("https://accounts.spotify.com/api/token",
     `grant_type=client_credentials&code=${encodeURIComponent(code)}&redirect_uri=${decodeURIComponent("https://randofy.vercel.app")}&client_id=${req.spotify._credentials.clientId}&client_secret=${req.spotify._credentials.clientSecret}`,
