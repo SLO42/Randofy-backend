114e44c1ccf2b9432e43591d83c0195a6647c69d
SLO42 <saolivei@student.42.us.org>
2020-12-29 00:17:27 -0600

    added refresh route and updated scopes

--- routes/login.js
+++ routes/login.js
@@ -8,7 +8,7 @@ var redirect_uri = process.env.NODE_ENV === 'dev' ? 'localhost:3000' : 'https://
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log(req.spotify._credentials.clientId)
-    var scopes = 'user-read-private user-read-email';
+    var scopes = 'user-read-private user-read-email playlist-modify-public';
     res.redirect('https://accounts.spotify.com/authorize' +
         '?response_type=code' +
         '&client_id=' + req.spotify._credentials.clientId +
--- routes/token.js
+++ routes/token.js
@@ -21,7 +21,30 @@ var axios = require('axios');
         // console.log("error", error.response);
         res.status(error.response.status).send(error.response.data)
     })
-
 });
+
+    //requires that the params that is sent is set as { params : { refresh : refresh_token }}
+  router.get('/refresh', function(req, res, next) {
+    const refresh = req.query.refresh_token;
+    // console.log("url: ", `grant_type=authorization_code&code=${encodeURIComponent(code)}&redirect_uri=${encodeURIComponent("https://randofy.vercel.app/")}&client_id=${req.spotify._credentials.clientId}&client_secret=${req.spotify._credentials.clientSecret}`);
+    axios.post("https://accounts.spotify.com/api/token",
+    `grant_type=refresh_token&refresh_token=${encodeURIComponent(refresh)}`,
+    {
+        'Authorization': 'Basic ' + Buffer.from(req.spotify._credentials.clientId + ":" + req.spotify._credentials.clientSecret).toString('base64'),
+        'Content_Type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Header': '*',
+        'Cache-Control': 'no-cache',
+    })
+    .then(response => {
+        // console.log("data", response.data);
+        res.status(200).send(response.data)
+    })
+    .catch(error => {
+        // console.log("error", error.response);
+        res.status(error.response.status).send(error.response.data)
+    })
+});
+
+
 
 module.exports = router;
