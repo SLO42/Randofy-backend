b0e70233fbca42dc5db5ef65104e84cde941e82d
SLO42 <saolivei@student.42.us.org>
2020-12-29 17:07:19 -0600

    updated and repaired token refresh

--- routes/token.js
+++ routes/token.js
@@ -27,16 +27,11 @@ const { response } = require('express');
     //requires that the params that is sent is set as { params : { refresh : refresh_token }}
   router.get('/refresh', function(req, res, next) {
     const refresh = req.query.refresh_token;
-    const encoded = Buffer.from(req.spotify._credentials.clientId + ":" + req.spotify._credentials.clientSecret).toString('base64')
-    console.log("refresh", refresh)
-    console.log("encoded", encoded)
-    console.log("client", req.spotify._credentials.clientId)
-    console.log("secret", req.spotify._credentials.clientSecret)
+    // const encoded = Buffer.from(req.spotify._credentials.clientId + ":" + req.spotify._credentials.clientSecret).toString('base64')
     // console.log("url: ", `grant_type=authorization_code&code=${encodeURIComponent(code)}&redirect_uri=${encodeURIComponent("https://randofy.vercel.app/")}&client_id=${req.spotify._credentials.clientId}&client_secret=${req.spotify._credentials.clientSecret}`);
     axios.post("https://accounts.spotify.com/api/token",
-    `grant_type=refresh_token&refresh_token=${encodeURIComponent(refresh)}`,
+    `grant_type=refresh_token&refresh_token=${refresh}&client_id=${req.spotify._credentials.clientId}&client_secret=${req.spotify._credentials.clientSecret}`,
     {
-        'Authorization': 'Basic ' + Buffer.from(req.spotify._credentials.clientId + ":" + req.spotify._credentials.clientSecret).toString('base64'),
         'Content_Type': 'application/x-www-form-urlencoded',
         'Access-Control-Allow-Header': '*',
         'Cache-Control': 'no-cache',
