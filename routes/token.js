d47c82a351ff84b61aa918ff99cbaeabe40856df
SLO42 <saolivei@student.42.us.org>
2020-12-29 00:21:43 -0600

    testing

--- routes/token.js
+++ routes/token.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+const { response } = require('express');
 
   /* GET home page. */
   router.get('/', function(req, res, next) {
@@ -26,6 +27,11 @@ var axios = require('axios');
     //requires that the params that is sent is set as { params : { refresh : refresh_token }}
   router.get('/refresh', function(req, res, next) {
     const refresh = req.query.refresh_token;
+    const encoded = Buffer.from(req.spotify._credentials.clientId + ":" + req.spotify._credentials.clientSecret).toString('base64')
+    console.log("refresh", refresh)
+    console.log("encoded", encoded)
+    console.log("client", req.spotify._credentials.clientId)
+    console.log("secret", req.spotify._credentials.clientSecret)
     // console.log("url: ", `grant_type=authorization_code&code=${encodeURIComponent(code)}&redirect_uri=${encodeURIComponent("https://randofy.vercel.app/")}&client_id=${req.spotify._credentials.clientId}&client_secret=${req.spotify._credentials.clientSecret}`);
     axios.post("https://accounts.spotify.com/api/token",
     `grant_type=refresh_token&refresh_token=${encodeURIComponent(refresh)}`,
