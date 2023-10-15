9bc519e877bc11b96faa6531a7e5c4ce87ea8426
Samuel Oliveira <11sosam11@gmail.com>
2023-10-15 17:02:43 -0500

    refactor(random): remove geoip

--- pages/api/random.js
+++ pages/api/random.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import geoip from "geoip-lite";
+// import geoip from "geoip-lite";
 import getRandomSearch from "../../lib/js/helpers/randomLib.mjs";
 import { spotifyApi } from "../src/spotify-api/SpotifyClient.mjs";
 
@@ -46,35 +46,35 @@ const getData = (req, max, callback) => {
           if (!tracks || tracks.total === 0) {
             return doit();
           }
-          var getIpInfo = function (ip) {
-            // IPV6 addresses can include IPV4 addresses
-            // So req.ip can be '::ffff:86.3.182.58'
-            // However geoip-lite returns null for these
-            if (ip.includes("::ffff:")) {
-              ip = ip.split(":").reverse()[0];
-            }
-            var lookedUpIP = geoip.lookup(ip);
-            if (ip === "127.0.0.1" || ip === "::1") {
-              return { error: "This won't work on localhost" };
-            }
-            if (!lookedUpIP) {
-              return {
-                error: "Error occured while trying to process the information",
-              };
-            }
-            return lookedUpIP;
-          };
+          // var getIpInfo = function (ip) {
+          //   // IPV6 addresses can include IPV4 addresses
+          //   // So req.ip can be '::ffff:86.3.182.58'
+          //   // However geoip-lite returns null for these
+          //   if (ip.includes("::ffff:")) {
+          //     ip = ip.split(":").reverse()[0];
+          //   }
+          //   var lookedUpIP = geoip.lookup(ip);
+          //   if (ip === "127.0.0.1" || ip === "::1") {
+          //     return { error: "This won't work on localhost" };
+          //   }
+          //   if (!lookedUpIP) {
+          //     return {
+          //       error: "Error occured while trying to process the information",
+          //     };
+          //   }
+          //   return lookedUpIP;
+          // };
 
-          var xForwardedFor = (req.headers["x-forwarded-for"] || "").replace(
-            /:\d+$/,
-            ""
-          );
-          var ip = xForwardedFor || req.connection.remoteAddress;
-          req.ipInfo = { ip, ...getIpInfo(ip) };
+          // var xForwardedFor = (req.headers["x-forwarded-for"] || "").replace(
+          //   /:\d+$/,
+          //   ""
+          // );
+          // var ip = xForwardedFor || req.connection.remoteAddress;
+          // req.ipInfo = { ip, ...getIpInfo(ip) };
           const item = tracks.items[0];
           if (
-            (item &&
-              item.album.available_markets.includes(req.ipInfo.country)) ||
+            item ||
+            // && item.album.available_markets.includes(req.ipInfo.country)
             (process.env.NODE_ENV === "development" && item)
           ) {
             // console.log(item.id);
